ea601f9e3cb8c872fff0a6c838e415f1f9a0ec1d
~Error~ <142939840+DarkPixieRuler@users.noreply.github.com>
2025-09-04 15:40:31 +0000

    CatchingUpToCeleste

---
+++ Newsite.js
@@ -0,0 +1,17 @@
+var response = prompt("Who are you mortal?")
+
+if (response === "HalleBotGod110") {
+  alert("Ahh I see you are god, you may enter.")
+
+}
+
+else if (response === "Architech Celeste") {
+  alert("GET OUT OF HERE MORTAL!!");
+  location.href="index.html";
+}
+else if (response === "Mr. Gaspard") {
+  alert("Ah, I see, welcome teacher");
+}
+else if (response === "Doom Guy") {
+  alert("A most respected mortal, I see, you may enter.");
+}
