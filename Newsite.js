1c2085de9421b56b4d070dc3738316aece19f833
~Error~ <142939840+DarkPixieRuler@users.noreply.github.com>
2025-09-08 15:49:46 +0000

    SmallTweaksUpdate

--- Newsite.js
+++ Newsite.js
@@ -1,11 +1,8 @@
 
 var response = prompt("Who are you mortal?")
-
 if (response === "HalleBot God") {
   alert("Ahh I see you are the god, you may enter my ruler.")
-
 }
-
 else if (response === "Architech Celeste") {
   alert("You do not deserve to be here, GET OUT OF HERE MORTAL!!");
   location.href="index.html";
